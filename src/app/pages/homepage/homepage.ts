80d5cdd96f37a0d46ae64976091f5d2ccb86d12b
Daniel Fabbri <contato.fabbri@gmail.com>
2025-09-25 09:28:35 -0300

    chore: rotas, layout, conteudo inicial; fix footer year; melhorias home

--- src/app/pages/homepage/homepage.ts
+++ src/app/pages/homepage/homepage.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Footer } from '../../components/footer/footer';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-homepage',
-  imports: [RouterLink],
+  imports: [RouterLink, Header, Footer],
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss'
 })
