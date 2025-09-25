97a78ece6316ac4c1a0f234cc817683674942660
Daniel Fabbri <contato.fabbri@gmail.com>
2025-09-25 11:10:00 -0300

    Zorro install

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
-import { Homepage } from './pages/homepage/homepage';
-import { Projects } from './pages/projects/projects';
-import { About } from './pages/about/about';
 
 export const routes: Routes = [
-  { path: '', component: Homepage, title: 'Início | Natalia Vianna' },
-  { path: 'projects', component: Projects, title: 'Projetos | Natalia Vianna' },
-  { path: 'about', component: About, title: 'Sobre | Natalia Vianna' },
-  { path: '**', redirectTo: '' }
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
-import { Footer } from './components/footer/footer';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, NzLayoutModule, NzMenuModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('nataliavianna');
-}
+export class App {}
