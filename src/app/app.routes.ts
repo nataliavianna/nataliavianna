b81294e5b3f4984b6dfad9f641b720a49035ad5f
Daniel Fabbri <contato.fabbri@gmail.com>
2025-09-26 08:43:27 -0300

    Optimizing content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
+import { About } from './pages/about/about';
+import { Projects } from './pages/projects/projects';
+import { Homepage } from './pages/homepage/homepage';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
+  { path: '', pathMatch: 'full', redirectTo: '/homepage' },
+  { path: 'homepage', component: Homepage },
+  { path: 'about', component: About },
+  { path: 'projects', component: Projects },
+  // { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
 ];
--- src/app/pages/homepage/homepage.ts
+++ src/app/pages/homepage/homepage.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+// import { Header } from '../../components/header/header';
 import { Footer } from '../../components/footer/footer';
-import { Header } from '../../components/header/header';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-homepage',
-  imports: [RouterLink, Header, Footer],
+  imports: [RouterLink, NzPageHeaderModule, NzGridModule, NzCardModule, NzTagModule, NzListModule, NzButtonModule, NzLayoutModule, Footer],
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss'
 })
 export class Homepage {
-
+  
+  achievements = [
+    { title: 'Product Manager', description: 'Lead cross-functional teams to deliver high-impact products' },
+    { title: 'SaaS', description: 'Scale SaaS products from MVP to enterprise-level solutions' },
+    { title: 'Fintech', description: 'Implement fintech features to enhance user experience and compliance' },
+    { title: 'Social Impact', description: 'Drive social impact initiatives to improve lives and communities' }
+  ];
 }
