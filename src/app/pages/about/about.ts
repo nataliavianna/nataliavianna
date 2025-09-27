53bddac25e20999640e2533cf62666f2f00f2401
Daniel Fabbri <contato.fabbri@gmail.com>
2025-09-27 13:15:02 -0300

    More from About and Projects Layout

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { Header } from '../../components/header/header';
 import { Footer } from '../../components/footer/footer';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
 
 @Component({
   selector: 'app-about',
-  imports: [Header, Footer],
+  imports: [RouterLink, NzPageHeaderModule, NzGridModule, NzCardModule, NzTagModule, NzListModule, NzButtonModule, NzLayoutModule, Header, Footer],
   templateUrl: './about.html',
   styleUrl: './about.scss'
 })
